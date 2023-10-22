d5464ac41ecbdc0a598e34269984b0fe8c8c1d71
CJxCJ <115903431+CJxCJ@users.noreply.github.com>
2023-10-22 16:47:07 -0500

    Update punto_10_taller_2.py

--- punto_10_taller_2.py
+++ punto_10_taller_2.py
@@ -1,10 +1,10 @@
 #funcion que crea una lista vacia y analiza si son o no multiplos de 3 los datos
 def multiplos_de_tres(lista):
-  multiplos = []
-  for num in lista:
-    if num % 3 == 0:
-      multiplos.append(num)
-  return multiplos
+    multiplos = []
+    for num in lista:
+      if num % 3 == 0:
+        multiplos.append(num)
+    return multiplos
 
 #pedir los numeros separados por espacio y hacer cada valor un dato independiente
 entrada = input("Ingresa los valores de la lista separados por espacios: ")
@@ -13,5 +13,4 @@ valores = [int(valor) for valor in entrada.split()]
 #usa la funcion en la lista creada
 multiplos = multiplos_de_tres(valores)
 
-#resultado
-print("Los múltiplos de 3 en la lista son:", multiplos_de_tres)
+print("Los múltiplos de 3 en la lista son:", multiplos)
